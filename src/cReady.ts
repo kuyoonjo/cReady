1bef91c4cc46423ba6ab4c2228e5b519b9278293
Yu Chen <yu.chen@live.ie>
2021-03-18 11:41:00 +0800

    fc.

--- src/cReady.ts
+++ src/cReady.ts
@@ -1,9 +1,9 @@
 export class cReady {
   private _resolve: any;
-  private _ready = new Promise<void>(r => this._resolve = r);
+  private _ready = new Promise<void>(r => (this._resolve = r));
   public setAsReady(): void {
     return this._resolve();
-  };
+  }
   public ready() {
     return this._ready;
   }
